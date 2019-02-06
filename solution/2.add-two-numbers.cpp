229af61c1e7cdccce305bbf49eed3d6aebedc846
姜旭 <jiangxu@jiangxus-MacBook-Pro.local>
2019-02-06 15:39:52 +0800

    Add new version of 2 solution

--- solution/2.add-two-numbers.cpp
+++ solution/2.add-two-numbers.cpp
@@ -38,6 +38,19 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        ListNode *dummy = new ListNode(-1);
+        ListNode *p = dummy;
+        bool carry = 0;
+        while(l1 || l2 || carry){
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = (sum >= 10);
+            if(carry)
+                sum -= 10;
+            p->next = new ListNode(sum);
+            p = p->next;
+            l1 = l1 ? l1->next : l1;
+            l2 = l2 ? l2->next : l2;
+        }
+        return dummy->next;
     }
 };
