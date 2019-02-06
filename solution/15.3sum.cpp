b786ac0488c3447031699971fdd5495519b89a2c
姜旭 <jiangxu@jiangxus-MacBook-Pro.local>
2019-02-06 15:15:30 +0800

    Use Vscode plugins

---
+++ solution/15.3sum.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=15 lang=cpp
+ *
+ * [15] 3Sum
+ *
+ * https://leetcode.com/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (23.08%)
+ * Total Accepted:    464.9K
+ * Total Submissions: 2M
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * Given an array nums of n integers, are there elements a, b, c in nums such
+ * that a + b + c = 0? Find all unique triplets in the array which gives the
+ * sum of zero.
+ * 
+ * Note:
+ * 
+ * The solution set must not contain duplicate triplets.
+ * 
+ * Example:
+ * 
+ * 
+ * Given array nums = [-1, 0, 1, 2, -1, -4],
+ * 
+ * A solution set is:
+ * [
+ * ⁠ [-1, 0, 1],
+ * ⁠ [-1, -1, 2]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;        
+        auto len = nums.size();
+
+        for(int i = 0; i < len; i++){
+            int target = -nums[i];
+            int front = i + 1;
+            int back = len - 1;
+
+            while(front < back){
+                int sum = nums[front] + nums[back];
+                if(target > sum)
+                    front++;
+                else if(target < sum)
+                    back--;
+                else{
+                    vector<int> tmp(3, 0);
+                    tmp[0] = nums[i];
+                    tmp[1] = nums[front];
+                    tmp[2] = nums[back];
+                    res.push_back(tmp);
+
+                    while(front < back && nums[front] == tmp[1])
+                        front++;
+                    while(front < back && nums[back] == tmp[2])
+                        back--;
+                }
+                while(i < len - 1 && nums[i+1] == nums[i])
+                    i++;
+            }
+        }
+        return res;
+    }
+};
---
+++ solution/20.valid-parentheses.cpp
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (35.67%)
+ * Total Accepted:    497.1K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+class Solution {
+public:
+    bool isValid(string s) {
+        int len = s.size();
+        stack<char> stack;
+        for(auto c: s){
+            if(c == '{')
+                stack.push('}');
+            else if(c == '(')
+                stack.push(')');
+            else if(c == '[')
+                stack.push(']');
+            else if(stack.empty() || stack.top() != c)
+                return false;
+            else
+                stack.pop();
+        }
+        return stack.empty();
+    }
+};
